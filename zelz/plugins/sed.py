a1ca6399be9581fa1a5e50d5d68420926e8c081c
test2440 <test2440t@gmail.com>
2025-08-20 19:49:40 +0300

    Add files via upload

---
+++ zelz/plugins/sed.py
@@ -0,0 +1,140 @@
+import json
+import math
+import os
+import aiohttp
+import requests
+import random
+import re
+import time
+from uuid import uuid4
+import sys
+import asyncio
+from validators.url import url
+from subprocess import run as runapp
+from datetime import datetime
+from pySmartDL import SmartDL
+from pathlib import Path
+from platform import python_version
+from telethon import Button, functions, events, types, custom
+from telethon.errors import QueryIdInvalidError
+from telethon.events import CallbackQuery, InlineQuery
+from telethon.utils import get_display_name
+from telethon.tl.types import InputMessagesFilterDocument, MessageEntityMentionName
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import BotInlineResult, InputBotInlineMessageMediaAuto, DocumentAttributeImageSize, InputWebDocument, InputBotInlineResult
+from telethon.tl.functions.messages import SetInlineBotResultsRequest
+
+from . import zedub
+from ..Config import Config
+from ..helpers.functions import rand_key
+from ..core.logger import logging
+from ..core.managers import edit_delete, edit_or_reply
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from ..helpers.utils import reply_id, _format
+from . import media_type, progress
+from ..utils import Zed_Dev, load_module, remove_plugin
+from ..sql_helper.global_collection import add_to_collectionlist, del_keyword_collectionlist, get_collectionlist_items
+from . import SUDO_LIST, edit_delete, edit_or_reply, reply_id, BOTLOG, BOTLOG_CHATID, HEROKU_APP, mention
+
+LOGS = logging.getLogger(os.path.basename(__name__))
+
+# نصوص جاهزة
+scc = "secret"
+hmm = "همسـة"
+ymm = "يستطيـع"
+fmm = "فتـح الهمسـه 🗳"
+dss = "⌔╎هو فقط من يستطيع ࢪؤيتهـا"
+hss = "ᯓ 𝗦𝗢𝗨𝗥𝗖𝗘 𝙈𝙖𝙏𝙍𝙞𝙭  - **همسـة سـࢪيـه** 📠\n⋆┄─┄─┄─┄┄─┄─┄─┄─┄┄⋆\n**⌔╎الهمسـة لـ**"
+nmm = "همسـه سريـه"
+mnn = "ارسـال همسـه سريـه لـ (شخـص/اشخـاص).\nعبـر ماتركـس"
+bmm = "اضغـط للـرد"
+ttt = "ᯓ 𝗦𝗢𝗨𝗥𝗖𝗘 𝙈𝙖𝙏𝙍𝙞𝙭  - همسـة سـࢪيـه\n⋆┄─┄─┄─┄┄─┄─┄─┄─┄┄⋆\n⌔╎اضغـط الـزر بالاسفـل ⚓\n⌔╎لـ اࢪسـال همسـه سـࢪيـه الى"
+ddd = "💌"
+Zel_Uid = zedub.uid
+
+# دالة تجيب معلومات المستخدم
+async def get_user_from_event(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_object = await event.client.get_entity(previous_message.sender_id)
+    else:
+        user = event.pattern_match.group(1)
+        if user.isnumeric():
+            user = int(user)
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        if isinstance(user, int) or user.startswith("@"):
+            user_obj = await event.client.get_entity(user)
+            return user_obj
+        try:
+            user_object = await event.client.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return user_object
+
+# دالة تجيب ID واسم المستخدم
+async def zzz_info(zthon_user, event):  # Write Code By Zelzal T.me/zzzzl1l
+    FullUser = (await event.client(GetFullUserRequest(zthon_user.id))).full_user
+    first_name = zthon_user.first_name
+    full_name = FullUser.private_forward_name
+    user_id = zthon_user.id
+    username = zthon_user.username
+    first_name = (
+        first_name.replace("\u2060", "")
+        if first_name
+        else None
+    )
+    full_name = full_name or first_name
+    username = "@{}".format(username) if username else "None"
+    return user_id, full_name, username
+
+# دالة أساسية للأوامر الثلاثة
+async def run_hmsah_command(event):
+    global bbb
+    if gvarstatus("ZThon_Vip") is None and Zel_Uid not in Zed_Dev:
+        return await edit_or_reply(event, "**⎉╎عـذࢪاً .. ؏ـزيـزي\n⎉╎هـذا الامـر ليـس مجـانـي📵\n⎉╎للاشتـراك في الاوامـر المدفوعـة\n⎉╎تواصـل مطـور السـورس @dev_blal**")
+    
+    user = event.pattern_match.group(1)
+    if not user and not event.reply_to_msg_id:
+        return
+    
+    zthon_user = await get_user_from_event(event)
+    try:
+        user_id, full_name, username = await zzz_info(zthon_user, event)
+    except (AttributeError, TypeError):
+        return
+    
+    delgvar("hmsa_id")
+    delgvar("hmsa_name")
+    delgvar("hmsa_user")
+    addgvar("hmsa_id", user_id)
+    addgvar("hmsa_name", full_name)
+    addgvar("hmsa_user", username)
+    
+    if gvarstatus("hmsa_id"):
+        bbb = [(Button.switch_inline("اضـغـط هنـا", query=("secret " + gvarstatus("hmsa_id") + " \nهلو"), same_peer=True))]
+    else:
+        bbb = [(Button.switch_inline("اضغـط هنـا", query=("secret " + gvarstatus("hmsa_id") + " \nهلو"), same_peer=True))]
+    
+    response = await zedub.inline_query(Config.TG_BOT_USERNAME, "zelzal")
+    await response[0].click(event.chat_id)
+    await event.delete()
+
+# ربط الأوامر الثلاثة بنفس الدالة
+@zedub.zed_cmd(pattern="همسه(?: |$)(.*)")
+async def cmd_hmsah(event):
+    await run_hmsah_command(event)
+
+@zedub.zed_cmd(pattern="اهمس(?: |$)(.*)")
+async def cmd_ahmes(event):
+    await run_hmsah_command(event)
+
+@zedub.zed_cmd(pattern="همسة(?: |$)(.*)")
+async def cmd_hamsah(event):
+    await run_hmsah_command(event)
